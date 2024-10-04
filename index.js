bf642d931f75ffb850604f5e2931bffe632461ba
sandra <Bellawritefashion@gmail.com>
2024-10-04 12:58:04 +0100

    latest commiit

--- index.js
+++ index.js
@@ -21,22 +21,27 @@ mobileClose.addEventListener('click', function() {
       const textDiv = document.createElement('div');
       let span = document.createElement('span');
       span.innerHTML='Easy shopping '
-
+      span.style.fontSize = '33.7px';
+      span.style.color="white";
       let span1 = document.createElement('span');
      span1.innerHTML = 'just from';
-     span1.style.fontSize = '29.5px';
+     span1.style.fontSize = '25.5px';
+     span1.style.alignItems="center";
+     span1.style.color="white";
      let span2 = document.createElement('span');
      span2.innerHTML = 'home';
      span2.style.color = 'rgb(255, 200, 47)';
       textDiv.appendChild(span);
       textDiv.innerHTML += '<br>';
       textDiv.appendChild(span1);
+      textDiv.innerHTML+=' ';
       textDiv.appendChild(span2);
       textDiv.style.position = 'absolute';  
       textDiv.style.width ='357.19px'
       textDiv.style.height='92px'
-      textDiv.style.top = '420.7px';  
-      textDiv.style.left = '62.7px';
+      textDiv.style.top = '340.7px';  
+      textDiv.style.textAlign='center'
+      textDiv.style.left = '30px';
       textDiv.style.rotate ='-5deg'
       textDiv.style.fontSize = '24px'; 
       textDiv.style.fontFamily = 'Arial'; 
